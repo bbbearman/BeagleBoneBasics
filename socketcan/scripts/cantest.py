2c9f480d268c08e15242995feb16cfce1ad4f44e
bearman.m <69101380+bbbearman@users.noreply.github.com>
2021-09-22 16:51:02 +0800

    Whole Architecture

--- socketcan/scripts/cantest.py
+++ socketcan/scripts/cantest.py
@@ -4,39 +4,124 @@ import can
 import os
 from threading import Timer
 
-#------Global Variables------#
-counter=0
+#====================================#
+#=======Global Variables Here========#
+#====================================#
+
+#Variables
+counter0=0
+counter1=0
+
+#Interrupt Timer
+send_timer=0.5          #triggered every 500ms
+receive_timer=0.2       #triggered every 200ms
+mainloop_timer=0.01    #triggered every 5ms
+
+#Received CAN dataframes
+
+receive_msg0 = can.message.Message(arbitration_id = 0x00, data = [0,0,0,0,0,0,0,0])
 
 
-#-------pack and send can frams------#
+receive_msg1 = 0
+#Message IDs
+
+
+#====================================#
+#=======pack and send can frames=====#
+#====================================#
 def can0send(MsgID):
     
-    global counter
+    global counter0
    
     bus = can.interface.Bus(channel='can0', bustype='socketcan')
-    msg = can.Message(arbitration_id=MsgID, data=[counter, 2, 3, 1, 3, 1, 4, 1], is_extended_id=False)
+    msg = can.Message(arbitration_id=MsgID, data=[counter0, 2, 3, 1, 3, 1, 4, 1], is_extended_id=False)
     bus.send(msg)
     
-    counter=counter+1
-    counter=counter%256
+    counter0=(counter0+1)%256
 
+def can1send(MsgID):
+    
+    global counter1
+   
+    bus = can.interface.Bus(channel='can1', bustype='socketcan')
+    msg = can.Message(arbitration_id=MsgID, data=[counter1, 2, 3, 1, 3, 1, 4, 1], is_extended_id=False)
+    bus.send(msg)
+    
+    counter1=(counter1+1)%256
+
+#====================================#
+#==========receive can frames========#
+#====================================#
+'''
+note that blocking mode will affect the execution speed of the main loop,
+so it must be placed in other threads(interrupts)
+'''
+def can0receive():
+    global receive_msg0
+
+    bus = can.interface.Bus(channel='can0', bustype='socketcan')
+    message = bus.recv(send_timer)    # 0 == non-blocking 
+    if message is not None:
+        receive_msg0 = message
+
+
+def can1receive():
+    global receive_msg1
+
+    bus = can.interface.Bus(channel='can1', bustype='socketcan')
+    message = bus.recv(send_timer)    # 0 == non-blocking
+
+    if message is not None:
+        receive_msg1 = message
+
+
+
+#====================================#
+#==============main loop=============#
+#====================================#
+def main():
+
+    #print('i can fresh very fast')
+  
+    print(receive_msg0)
+    #print(receive_msg0.arbitration_id)
+    #print(receive_msg0.data[0])
+
+
+#-----Configure the Interrupt-----#
 class RepeatingTimer(Timer):
     def run(self):
         while not self.finished.is_set():
             self.function(*self.args, **self.kwargs)
             self.finished.wait(self.interval)
 
+
+
+#====================================#
+#===========Initialization===========#
+#====================================#
+
 if __name__ == '__main__':
 
-    print('Pin Configuring')
+
+    #---------Pin Configuration Here---------#
+
+    print('Pin Configuring......')
     os.system('sh /home/debian/myProject/caninit_loopbackon')
-    print('Pin Configure Finished')
+    #os.system('sh /home/debian/myProject/caninit_loopbackoff')
+    print('Pin Configure Finished !')
 
     print('sending can message now')
-    t = RepeatingTimer(1 , can0send,(0x300,))
-    t.start()
-    
-    while True:
-        print('main loop run every 1 second now')
-        time.sleep(1)
-    
+
+    #--------start can timers--------------------#
+
+    SEND0 = RepeatingTimer(send_timer , can0send,(0x300,))
+    SEND0.start()
+
+    RECEIVE0 = RepeatingTimer(send_timer , can0receive)
+    RECEIVE0.start()
+
+    #--------start main timer----------------#
+    print('main loop run now')
+    MAINLOOP = RepeatingTimer(mainloop_timer , main)
+    MAINLOOP.start()
